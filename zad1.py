cd8577e2fcabb618d21947c9c529418905d30f28
PavelVorobyev77 <pascha27_05@mail.ru>
2024-05-09 15:43:30 +0700

    исправил код! выводит формат (переменная, мин, макс)

--- zad1.py
+++ zad1.py
@@ -11,21 +11,23 @@ def read_log_file(file_path):
 
 # Функция для нахождения минимального и максимального значений в словаре
 def find_min_max(dictionary_of_data_from_file):
-    min_val = min(dictionary_of_data_from_file.values())
-    max_val = max(dictionary_of_data_from_file.values())
-    min_names = [key for key, val in dictionary_of_data_from_file.items() if val == min_val]
-    max_names = [key for key, val in dictionary_of_data_from_file.items() if val == max_val]
-    return min_val, min_names, max_val, max_names
+    min_max_values = {}
+    for variable, values in dictionary_of_data_from_file.items():
+        min_val = min(values)
+        max_val = max(values)
+        min_max_values[variable] = (min_val, max_val)
+    return min_max_values
 
+# Функция для записи статистики в выходной файл
 # Функция для записи статистики в выходной файл
 def write_statistics(day, year, category_contents, file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(f'{day} {year}\n')
-        # Нахождение минимального и максимального значений во всем файле
-        min_val, min_names, max_val, max_names = find_min_max({item: count for contents in category_contents.values() for item, count in contents.items()})
-        file.write(f' MIN\t\t{min_val}\t\t{" ".join(min_names)}\n')
-        file.write(f' MAX\t\t{max_val}\t\t{" ".join(max_names)}\n')
-
+        file.write('Variable\tMIN\tMAX\n')
+        # Нахождение минимального и максимального значений для каждой переменной
+        min_max_values = find_min_max({item: count for contents in category_contents.values() for item, count in contents.items()})
+        for variable, (min_val, max_val) in min_max_values.items():
+            file.write(f'{variable:<10}\t{min_val}\t{max_val}\n')
 
 file_input = input('Выберите входной файл: ') #"C:/Users/pasch/Desktop/sample.txt"
 file_output = input('Выберите выходной файл: ') #"C:/Users/pasch/Desktop/output_file.txt"
@@ -65,14 +67,14 @@ for line in lines:
     # Если строка содержит данные, сохраняем их в словаре
     if words[1] == '-':
         if f'{words[0]} -' in info_from_file[category_name]:
-            info_from_file[category_name][f'{words[0]} -'] += int(words[2])
+            info_from_file[category_name][f'{words[0]} -'].append(int(words[2]))
         else:
-            info_from_file[category_name][f'{words[0]} -'] = int(words[2])
+            info_from_file[category_name][f'{words[0]} -'] = [int(words[2])]
     else:
         if words[0] in info_from_file[category_name]:
-            info_from_file[category_name][words[0]] += int(words[1])
+            info_from_file[category_name][words[0]].append(int(words[1]))
         else:
-            info_from_file[category_name][words[0]] = int(words[1])
+            info_from_file[category_name][words[0]] = [int(words[1])]
 
 write_statistics(day, year, info_from_file, file_output)
 print(f'Данные были успешно записаны в файл {file_output}')
