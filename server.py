140ec59834b075c650035c9856531c75e670efaf
PavelVorobyev77 <pascha27_05@mail.ru>
2024-05-10 20:42:06 +0700

    можно теперь делать поиск статистики по дате!

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
@@ -20,12 +21,46 @@ def get_data():
     cur.close()
     return data
 
+# Функция для поиска данных по дате
+def search_data(date):
+    cur = conn.cursor()
+    cur.execute("SELECT Variable, min, max, Date FROM statistics_sample WHERE Date = %s", (date,))
+    data = [{'Variable': row[0], 'min': row[1], 'max': row[2], 'Date': row[3]} for row in cur.fetchall()]
+    cur.close()
+    return data
+
+
+# Функция для поиска данных по диапазону дат
+def search_data_range(date1, date2):
+    cur = conn.cursor()
+    cur.execute("SELECT Variable, SUM(min), SUM(max) FROM statistics_sample WHERE Date BETWEEN %s AND %s GROUP BY Variable", (date1, date2))
+    data = [{'Variable': row[0], 'min': row[1], 'max': row[2]} for row in cur.fetchall()]
+    cur.close()
+    return data
 
 # Маршрут для главной страницы
 @app.route('/')
 def index():
     data = get_data()
     return render_template('page.html', data=data)
+
+# Маршрут для поиска по дате
+@app.route('/search', methods=['POST'])
+def search():
+    date_str = request.form.get('date')
+    date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    data = search_data(date_obj)
+    return render_template('page.html', data=data)
+
+# Маршрут для поиска по диапазону дат
+@app.route('/search_range', methods=['POST'])
+def search_range():
+    date1_str = request.form.get('date1')
+    date2_str = request.form.get('date2')
+    date1_obj = datetime.datetime.strptime(date1_str, '%Y-%m-%d')
+    date2_obj = datetime.datetime.strptime(date2_str, '%Y-%m-%d')
+    data = search_data_range(date1_obj, date2_obj)
+    return render_template('page.html', data=data, date1=date1_obj, date2=date2_obj)
 
 if __name__ == '__main__':
     app.run(debug=True)
