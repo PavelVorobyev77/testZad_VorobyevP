a658e7f7bc97227cdecc8137ac9bf53e91510950
PavelVorobyev77 <pascha27_05@mail.ru>
2024-05-17 19:48:51 +0700

    Переделал server под ORACLE, будь она проклята...

--- server.py
+++ server.py
@@ -1,40 +1,35 @@
 import datetime
 from flask import Flask, render_template, request
-import psycopg2
+import cx_Oracle
+cx_Oracle.init_oracle_client(lib_dir=r"C:\ORACLE\instantclient_21_13")
 
 app = Flask(__name__)
 
 # Подключение к базе данных
-conn = psycopg2.connect(
-    dbname="pr_practice_VorobyevP",
-    user="postgres",
-    password="admin",
-    host="localhost",
-    port="5432"
-)
+dsn = cx_Oracle.makedsn('localhost', 1521, service_name='xe')
+conn = cx_Oracle.connect(user='system', password='Admin', dsn=dsn)
 
 # Функция для получения данных из базы данных
 def get_data():
     cur = conn.cursor()
-    cur.execute("SELECT Date, Variable, min, max FROM statistics_sample")
-    data = [{'Date': row[0], 'Variable': row[1], 'min': row[2], 'max': row[3]} for row in cur.fetchall()]
+    cur.execute("SELECT Sample_Date, Variable, min_value, max_value FROM statistics_sample")
+    data = [{'Sample_Date': row[0], 'Variable': row[1], 'min_value': row[2], 'max_value': row[3]} for row in cur.fetchall()]
     cur.close()
     return data
 
 # Функция для поиска данных по дате
 def search_data(date):
     cur = conn.cursor()
-    cur.execute("SELECT Variable, min, max, Date FROM statistics_sample WHERE Date = %s", (date,))
-    data = [{'Variable': row[0], 'min': row[1], 'max': row[2], 'Date': row[3]} for row in cur.fetchall()]
+    cur.execute("SELECT Variable, min_value, max_value, Sample_Date FROM statistics_sample WHERE Sample_Date = :1", (date,))
+    data = [{'Variable': row[0], 'min_value': row[1], 'max_value': row[2], 'Sample_Date': row[3]} for row in cur.fetchall()]
     cur.close()
     return data
-
 
 # Функция для поиска данных по диапазону дат
 def search_data_range(date1, date2):
     cur = conn.cursor()
-    cur.execute("SELECT Variable, SUM(min), SUM(max) FROM statistics_sample WHERE Date BETWEEN %s AND %s GROUP BY Variable ORDER BY Variable", (date1, date2))
-    data = [{'Variable': row[0], 'min': row[1], 'max': row[2]} for row in cur.fetchall()]
+    cur.execute("SELECT Variable, SUM(min_value), SUM(max_value) FROM statistics_sample WHERE Sample_Date BETWEEN :1 AND :2 GROUP BY Variable", (date1, date2))
+    data = [{'Variable': row[0], 'min_value': row[1], 'max_value': row[2]} for row in cur.fetchall()]
     cur.close()
     return data
 
@@ -68,7 +63,5 @@ def reset():
     data = get_data()
     return render_template('page.html', data=data)
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
